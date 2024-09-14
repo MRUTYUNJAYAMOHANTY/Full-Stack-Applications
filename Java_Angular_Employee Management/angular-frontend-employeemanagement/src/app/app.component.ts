bda2d5752571cac82523713d90c4ea829c17ff09
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2024-09-14 19:36:16 +0530

    CRUD

--- Java_Angular_Employee Management/angular-frontend-employeemanagement/src/app/app.component.ts
+++ Java_Angular_Employee Management/angular-frontend-employeemanagement/src/app/app.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-frontend-employeemanagement';
+  title = 'Angular 18 Spring Boot CRUD Full Stack App';
 }
